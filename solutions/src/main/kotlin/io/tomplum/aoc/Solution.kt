247de5dafec820e7ea67378361900fd691b55be4
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-01 11:54:10 +0000

    Solutions | Added Solution Interface Default Function Behaviour

--- solutions/src/main/kotlin/io/tomplum/aoc/Solution.kt
+++ solutions/src/main/kotlin/io/tomplum/aoc/Solution.kt
@@ -1,9 +1,11 @@
 package io.tomplum.aoc
 
+import kotlin.UnsupportedOperationException
+
 /**
  * The solution to both parts of a given day in Advent of Code.
  */
 interface Solution<T> {
-    fun part1(): T
-    fun part2(): T
+    fun part1(): T = throw UnsupportedOperationException("Part 1 not yet implemented.")
+    fun part2(): T = throw UnsupportedOperationException("Part 2 not yet implemented.")
 }
