66d4a1b7526474e2786fd0a752c0542a18136e71
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-01 18:54:32 +0000

    Solutions | Added Execution Time Tracking / Logging to SolutionRunner

--- solutions/src/main/kotlin/io/tomplum/aoc/SolutionRunner.kt
+++ solutions/src/main/kotlin/io/tomplum/aoc/SolutionRunner.kt
@@ -1,15 +1,35 @@
 package io.tomplum.aoc
 
+import kotlin.system.measureTimeMillis
+
 class SolutionRunner {
     companion object {
         fun run(solution: Solution<*>) {
             //TODO: Replace println w/Logger once imported
             //TODO: Output the runtime of each solution? Change this to varargs
-            println("${solution.javaClass.simpleName} Solutions:")
-            val first = solution.part1()
-            println("Part 1: $first")
-            val second = solution.part2()
-            println("Part 2: $second")
+            println("${solution.javaClass.simpleName} Solutions:\n")
+            val firstTime = measureTimeMillis {
+                val first = solution.part1()
+                println("Part 1: $first")
+            }
+            println(formatExecutionTime(firstTime))
+
+            println("")
+
+            val secondTime = measureTimeMillis {
+                val second = solution.part2()
+                println("Part 2: $second")
+            }
+            println(formatExecutionTime(secondTime))
+
+            println("\nTotal ${formatExecutionTime(firstTime + secondTime)}")
         }
+
+        private fun formatExecutionTime(milliseconds: Long): String {
+            val s = milliseconds / 1000
+            val ms = milliseconds % 1000
+            return "Execution Time: ${s}s ${ms}ms"
+        }
+
     }
 }
