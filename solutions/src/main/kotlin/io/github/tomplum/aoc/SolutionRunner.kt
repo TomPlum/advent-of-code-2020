d40d49d25ad0b7b73c70e50f70d76257c4c7ba99
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-02 13:21:33 +0000

    Solutions | Solution Runner - Reduced Redundancy

--- solutions/src/main/kotlin/io/github/tomplum/aoc/SolutionRunner.kt
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/SolutionRunner.kt
@@ -5,32 +5,29 @@ import kotlin.system.measureTimeMillis
 class SolutionRunner {
     companion object {
         //TODO: Replace println w/Logger once imported
+        //TODO: Serialise runtimes and then read to report deltas
         fun run(vararg solutions: Solution<*>) {
             println("- Advent of Code 2020 Solution Report -\n")
 
             val runtime = solutions.map { solution ->
                 println("[${solution.javaClass.simpleName}]")
-                val firstTime = measureTimeMillis {
-                    val first = solution.part1()
-                    println("Part 1: $first")
-                }
-                println(formatExecutionTime(firstTime))
-
-                val secondTime = measureTimeMillis {
-                    val second = try {
-                        "Part 2: ${solution.part2()}"
-                    } catch (e: UnsupportedOperationException) {
-                        e.message
-                    }
-                    println(second)
-                }
-                println(formatExecutionTime(secondTime))
-
-                firstTime + secondTime
+                runPart(solution, Part.ONE) + runPart(solution, Part.TWO)
             }.sum()
 
 
             println("Total ${formatExecutionTime(runtime)}")
+        }
+
+        private fun runPart(solution: Solution<*>, part: Part): Long {
+            val runtime = measureTimeMillis {
+                val answer = when(part) {
+                    Part.ONE -> solution.part1()
+                    Part.TWO -> solution.part2()
+                }
+                println("Part ${part.value}: $answer")
+            }
+            println(formatExecutionTime(runtime))
+            return runtime
         }
 
         private fun formatExecutionTime(milliseconds: Long): String {
@@ -39,5 +36,6 @@ class SolutionRunner {
             return "Execution Time: ${s}s ${ms}ms\n"
         }
 
+        private enum class Part(val value: String) { ONE("1"), TWO("2") }
     }
 }
