96452c5665bf2d9afe5d173df0f43ffebb67be46
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-02 09:14:56 +0000

    Day 2 | Part 1 - Implemented Password Database Validator

--- implementation/src/main/kotlin/io/github/tomplum/aoc/password/PasswordDatabase.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/password/PasswordDatabase.kt
@@ -3,9 +3,10 @@ package io.github.tomplum.aoc.password
 class PasswordDatabase {
     val passwords = mutableListOf<Pair<String, CorporatePolicy>>()
 
-   /* fun validate(): List<String> = passwords.filter { (password, policy) ->
-
-    }*/
+    fun validate(): Int = passwords.filter { (password, policy) ->
+        val occurrences = password.filter { it == policy.mandatoryCharacter }.length
+        occurrences >= policy.minimumOccurrences && occurrences <= policy.maximumOccurrences
+    }.count()
 
     fun import(rawData: List<String>) = rawData.map { it.split(":") }.map { pair ->
         Pair(pair[1].trim(), CorporatePolicy(pair[0]))
--- implementation/src/test/kotlin/io/github/tomplum/aoc/password/PasswordDatabaseTest.kt
+++ implementation/src/test/kotlin/io/github/tomplum/aoc/password/PasswordDatabaseTest.kt
@@ -8,14 +8,15 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class PasswordDatabaseTest {
+    private val exampleData = TestInputReader().readInputAsString("password/example-database-list.txt")
+
     @Nested
     inner class Import {
         private val database = PasswordDatabase()
 
         @Test
         fun exampleData() {
-            val data = TestInputReader().readInputAsString("password/example-database-list.txt")
-            database.import(data.value)
+            database.import(exampleData.value)
             assertThat(database.passwords).isEqualTo(listOf(
                 Pair("abcde", CorporatePolicy("1-3 a")),
                 Pair("cdefg", CorporatePolicy("1-3 b")),
@@ -27,6 +28,16 @@ class PasswordDatabaseTest {
         fun emptyData() {
             database.import(emptyList())
             assertThat(database.passwords).isEmpty()
+        }
+    }
+
+    @Nested
+    inner class Validate {
+        @Test
+        fun exampleData() {
+            val database = PasswordDatabase()
+            database.import(exampleData.value)
+            assertThat(database.validate()).isEqualTo(2)
         }
     }
 }
