237464eebc22984c83e79e777f4aae9babe10a85
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-04 16:10:17 +0000

    Day 4 | Passport Field - Reduced Int Range Syntax Verbosity

--- implementation/src/main/kotlin/io/github/tomplum/aoc/passport/PassportField.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/passport/PassportField.kt
@@ -3,23 +3,23 @@ package io.github.tomplum.aoc.passport
 enum class PassportField {
     BIRTH_YEAR {
         override fun isValid(value: String): Boolean {
-            return value.length == 4 && value.toInt() in IntRange(1920, 2002)
+            return value.length == 4 && value.toInt() in 1920..2002
         }
     },
     ISSUE_YEAR {
         override fun isValid(value: String): Boolean {
-            return value.length == 4 && value.toInt() in IntRange(2010, 2020)
+            return value.length == 4 && value.toInt() in 2010..2020
         }
     },
     EXPIRATION_YEAR {
         override fun isValid(value: String): Boolean {
-            return value.length == 4 && value.toInt() in IntRange(2020, 2030)
+            return value.length == 4 && value.toInt() in 2020..2030
         }
     },
     HEIGHT {
         override fun isValid(value: String) = when(value.takeLast(2)) {
-            "cm" -> value.dropLast(2).toInt() in IntRange(150, 193)
-            "in" -> value.dropLast(2).toInt() in IntRange(59, 76)
+            "cm" -> value.dropLast(2).toInt() in 150..193
+            "in" -> value.dropLast(2).toInt() in 59..76
             else -> false
         }
     },
@@ -27,13 +27,13 @@ enum class PassportField {
         override fun isValid(value: String): Boolean {
             val letters = value.filter { it.isLetter() }
             val other = value.drop(1).filterNot { it.isLetter() }
-            val isHex = letters.all { it.toInt() in IntRange(97, 102) } && other.all { it.isDigit() }
+            val isHex = letters.all { it.toInt() in 97..102 } && other.all { it.isDigit() }
             return value.length == 7 && value.first() == '#' && isHex
         }
     },
     EYE_COLOUR {
         override fun isValid(value: String): Boolean {
-            return listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(value)
+            return value in listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
         }
     },
     PASSPORT_ID {
