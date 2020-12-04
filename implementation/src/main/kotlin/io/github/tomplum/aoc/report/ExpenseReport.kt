56b938ccb2e536bbaf22db03118ebdd8ca86ea45
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-04 16:09:50 +0000

    Day 1 | Part 1 - Increased Algorithm Naivety! Vastly Improved Runtime Performance

--- implementation/src/main/kotlin/io/github/tomplum/aoc/report/ExpenseReport.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/report/ExpenseReport.kt
@@ -1,10 +1,5 @@
 package io.github.tomplum.aoc.report
 
-import io.github.tomplum.aoc.extensions.cartesianProductCubic
-import io.github.tomplum.aoc.extensions.cartesianProductQuadratic
-import io.github.tomplum.aoc.extensions.product
-import io.github.tomplum.aoc.extensions.sum
-
 /**
  * The Elves in accounting need to fix your expense report; apparently, something isn't quite adding up.
  * Specifically, they need to find n entries that sum to 2020 and then multiply those two numbers together.
@@ -16,17 +11,17 @@ class ExpenseReport(private val data: List<Int>) {
      * Finds the product of the two [data] values whose sum is equal to the target value.
      * @see isTargetSum
      */
-    fun validateContainsTwoEntries(): Int = data.cartesianProductQuadratic()
-            .find { values -> values.sum().isTargetSum()}?.product()
-            ?: throw IllegalStateException("The report is already valid.")
+    fun validateContainsTwoEntries(): Int = data.flatMap { i ->
+        data.map { j -> if ((i + j).isTargetSum()) i * j else null }
+    }.filterNotNull().first()
 
     /**
      * Finds the product of the three [data] values whose sum is equal to the target value.
      * @see isTargetSum
      */
-    fun validateContainsThreeEntries(): Int = data.cartesianProductCubic()
-            .find { values -> values.sum().isTargetSum()}?.product()
-            ?: throw IllegalStateException("The report is already valid.")
+    fun validateContainsThreeEntries(): Int = data.flatMap { i -> data.flatMap { j ->
+        data.map { k -> if ((i + j + k).isTargetSum()) i * j * k else null
+    }}}.filterNotNull().first()
 
     private fun Int.isTargetSum(): Boolean = this == 2020
 }
