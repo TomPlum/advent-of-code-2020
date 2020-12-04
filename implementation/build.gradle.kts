92d994fb058a66db41a6aa6dd44181556a74ee86
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-04 15:23:01 +0000

    Gradle | Added JaCoCo Integration w/Implementation Sub-Projects

--- implementation/build.gradle.kts
+++ implementation/build.gradle.kts
@@ -1,8 +1,50 @@
 apply(from = "$rootDir/gradle/testing-dependencies.gradle.kts")
 apply(from = "$rootDir/gradle/logging-dependencies.gradle.kts")
 
+plugins {
+    jacoco
+}
+
 dependencies {
     //Gradle Sub-Project Dependencies
     implementation(project(":implementation:common"))
     testImplementation(project(":implementation:test-support"))
+}
+
+subprojects {
+    apply(plugin = "jacoco")
+
+    tasks.test {
+        finalizedBy(tasks.jacocoTestReport)
+    }
+
+    tasks.jacocoTestReport {
+        dependsOn(tasks.test)
+    }
+
+    jacoco {
+        toolVersion = "0.8.5"
+        reportsDir = file("$buildDir/reports")
+    }
+
+    tasks.jacocoTestReport {
+        group = "Reporting"
+        description = "Generate Jacoco test coverage report"
+
+        reports {
+            xml.isEnabled = true
+            html.isEnabled = true
+            csv.isEnabled = false
+        }
+    }
+
+    tasks.jacocoTestCoverageVerification  {
+        violationRules {
+            rule {
+                limit {
+                    minimum = "0.6".toBigDecimal()
+                }
+            }
+        }
+    }
 }
