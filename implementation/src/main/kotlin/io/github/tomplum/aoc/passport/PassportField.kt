bdbc17095671df2d851bfa6960fef36d1214ee46
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-04 11:58:32 +0000

    Day 4 | Part 2 - Implemented PasswordField Validation

--- implementation/src/main/kotlin/io/github/tomplum/aoc/passport/PassportField.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/passport/PassportField.kt
@@ -1,14 +1,39 @@
 package io.github.tomplum.aoc.passport
 
 enum class PassportField(val code: String) {
-    BIRTH_YEAR("byr"),
-    ISSUE_YEAR("iyr"),
-    EXPIRATION_YEAR("eyr"),
-    HEIGHT("hgt"),
-    HAIR_COLOUR("hcl"),
-    EYE_COLOUR("ecl"),
-    PASSPORT_ID("pid"),
-    COUNTRY_ID("cid");
+    BIRTH_YEAR("byr") {
+        override fun isValid(value: String) = value.length == 4 && value.toInt() in IntRange(1920, 2002)
+    },
+    ISSUE_YEAR("iyr") {
+        override fun isValid(value: String) = value.length == 4 && value.toInt() in IntRange(2010, 2020)
+    },
+    EXPIRATION_YEAR("eyr") {
+        override fun isValid(value: String) = value.length == 4 && value.toInt() in IntRange(2020, 2030)
+    },
+    HEIGHT("hgt") {
+        override fun isValid(value: String) = when(value.takeLast(2)) {
+            "cm" -> value.dropLast(2).toInt() in IntRange(150, 193)
+            "in" -> value.dropLast(2).toInt() in IntRange(59, 76)
+            else -> false
+        }
+    },
+    HAIR_COLOUR("hcl") {
+        override fun isValid(value: String): Boolean {
+            val isHex = value.filter { it.isLetter() }.all { it.toInt() in IntRange(97, 102) }
+            return value.length == 7 && value.first() == '#' && isHex
+        }
+    },
+    EYE_COLOUR("ecl") {
+        override fun isValid(value: String) = listOf("amb", "blu", "brn,", "gry", "grn", "hzl", "oth").contains(value)
+    },
+    PASSPORT_ID("pid") {
+        override fun isValid(value: String) = value.length == 9 && value.all { it.isDigit() }
+    },
+    COUNTRY_ID("cid") {
+        override fun isValid(value: String) = true
+    };
+
+    abstract fun isValid(value: String): Boolean
 
     companion object {
         fun fromString(code: String): PassportField = when(code) {
---
+++ implementation/src/test/kotlin/io/github/tomplum/aoc/passport/PassportFieldTest.kt
@@ -0,0 +1,138 @@
+package io.github.tomplum.aoc.passport
+
+import assertk.assertThat
+import assertk.assertions.isFalse
+import assertk.assertions.isTrue
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EmptySource
+import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.ValueSource
+
+class PassportFieldTest {
+    @Nested
+    inner class FieldValidity {
+        @Nested
+        inner class BirthYear {
+            @ParameterizedTest
+            @ValueSource(strings = ["2002", "1920", "1970"])
+            fun valid(value: String) {
+                assertThat(PassportField.BIRTH_YEAR.isValid(value)).isTrue()
+            }
+
+            @ParameterizedTest
+            @EmptySource
+            @ValueSource(strings = ["2003", "1919", "204"])
+            fun invalid(value: String) {
+                assertThat(PassportField.BIRTH_YEAR.isValid(value)).isFalse()
+            }
+        }
+
+        @Nested
+        inner class IssueYear {
+            @ParameterizedTest
+            @ValueSource(strings = ["2010", "2020", "2015"])
+            fun valid(value: String) {
+                assertThat(PassportField.ISSUE_YEAR.isValid(value)).isTrue()
+            }
+
+            @ParameterizedTest
+            @EmptySource
+            @ValueSource(strings = ["2009", "2021", "210"])
+            fun invalid(value: String) {
+                assertThat(PassportField.ISSUE_YEAR.isValid(value)).isFalse()
+            }
+        }
+
+        @Nested
+        inner class ExpirationYear {
+            @ParameterizedTest
+            @ValueSource(strings = ["2020", "2030", "2025"])
+            fun valid(value: String) {
+                assertThat(PassportField.EXPIRATION_YEAR.isValid(value)).isTrue()
+            }
+
+            @ParameterizedTest
+            @EmptySource
+            @ValueSource(strings = ["2019", "2031", "202"])
+            fun invalid(value: String) {
+                assertThat(PassportField.EXPIRATION_YEAR.isValid(value)).isFalse()
+            }
+        }
+
+        @Nested
+        inner class Height {
+            @ParameterizedTest
+            @ValueSource(strings = ["150cm", "167cm", "193cm", "59in", "65in", "76in"])
+            fun valid(value: String) {
+                assertThat(PassportField.HEIGHT.isValid(value)).isTrue()
+            }
+
+            @ParameterizedTest
+            @EmptySource
+            @ValueSource(strings = ["149cm", "194cm", "176", "58in", "77in", "65"])
+            fun invalid(value: String) {
+                assertThat(PassportField.HEIGHT.isValid(value)).isFalse()
+            }
+        }
+
+        @Nested
+        inner class HairColour {
+            @ParameterizedTest
+            @ValueSource(strings = ["#000000", "#ffffff", "#f1f1f1"])
+            fun valid(value: String) {
+                assertThat(PassportField.HAIR_COLOUR.isValid(value)).isTrue()
+            }
+
+            @ParameterizedTest
+            @EmptySource
+            @ValueSource(strings = ["#", "ffffff", "#1f1f1", "#12345g"])
+            fun invalid(value: String) {
+                assertThat(PassportField.HAIR_COLOUR.isValid(value)).isFalse()
+            }
+        }
+
+        @Nested
+        inner class EyeColour {
+            @ParameterizedTest
+            @ValueSource(strings = ["amb", "blu", "brn,", "gry", "grn", "hzl", "oth"])
+            fun valid(value: String) {
+                assertThat(PassportField.EYE_COLOUR.isValid(value)).isTrue()
+            }
+
+            @ParameterizedTest
+            @EmptySource
+            @ValueSource(strings = ["ghj", "ambd", "wat"])
+            fun invalid(value: String) {
+                assertThat(PassportField.EYE_COLOUR.isValid(value)).isFalse()
+            }
+        }
+
+        @Nested
+        inner class PassportID {
+            @ParameterizedTest
+            @ValueSource(strings = ["000000001", "123456789", "010101010"])
+            fun valid(value: String) {
+                assertThat(PassportField.PASSPORT_ID.isValid(value)).isTrue()
+            }
+
+            @ParameterizedTest
+            @EmptySource
+            @ValueSource(strings = ["12345678", "12345678f", "1234567890"])
+            fun invalid(value: String) {
+                assertThat(PassportField.PASSPORT_ID.isValid(value)).isFalse()
+            }
+        }
+
+        @Nested
+        inner class CountryID {
+            @ParameterizedTest
+            @EmptySource
+            @ValueSource(strings = ["100","56234"])
+            fun valid(value: String) {
+                assertThat(PassportField.COUNTRY_ID.isValid(value)).isTrue()
+            }
+        }
+    }
+}
