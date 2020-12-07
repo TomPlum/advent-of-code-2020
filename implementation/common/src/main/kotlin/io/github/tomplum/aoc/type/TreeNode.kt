e6d5d5e44bc832a489cad04eb085a379654babc7
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-07 17:04:53 +0000

    Day 7 | Luggage Rule Parser Fixed (Proved w/Example)

--- implementation/common/src/main/kotlin/io/github/tomplum/aoc/type/TreeNode.kt
+++ implementation/common/src/main/kotlin/io/github/tomplum/aoc/type/TreeNode.kt
@@ -5,10 +5,19 @@ open class TreeNode<T>(val value: T) {
 
     var children: MutableList<TreeNode<T>> = mutableListOf()
 
+    var depth = 0
+
     fun addChild(node: TreeNode<T>) {
+        node.depth = this.depth + 1
         children.add(node)
         node.parents.add(this)
     }
+
+    fun allChildren(): Set<TreeNode<T>> = children.toSet() + children.flatMap { it.allChildren() }
+
+    fun allParents(): Set<TreeNode<T>> = parents.toSet() + parents.flatMap { it.allParents() }
+
+    fun isRoot(): Boolean = parents.size == 0
 
     open fun isLeaf(): Boolean = children.isEmpty()
 
--- implementation/src/main/kotlin/io/github/tomplum/aoc/airport/luggage/LuggageRuleParser.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/airport/luggage/LuggageRuleParser.kt
@@ -2,62 +2,32 @@ package io.github.tomplum.aoc.airport.luggage
 
 class LuggageRuleParser private constructor() {
     companion object {
-        fun parse(rules: List<String>): LuggageNode {
+        fun parse(rules: List<String>): Set<LuggageNode> {
             val existing = mutableListOf<LuggageNode>()
-            val nodes = rules.map { it.replace(" bags", "").replace(" bag", "") }.map { rule ->
+
+            return rules.map { it.replace(" bags", "").replace(" bag", "") }.map { rule ->
                 val info = rule.split(" contain ")
                 val parentColour = info[0]
                 val data = info[1].dropLast(1).split(", ").filter { it != "no other" }
-                data.fold(LuggageNode(LuggageData(parentColour))) { parent, datum ->
+                val existingChildren = existing.flatMap { it.allChildren() }.map { it as LuggageNode }.distinct()
+                val potentialNode = LuggageNode(LuggageData(parentColour))
+                val parentNode = existingChildren.find { it == potentialNode } ?: potentialNode
+
+                val node = data.fold(parentNode) { parent, datum ->
                     parent.apply {
                         val childColour = datum.substring(2)
                         val childQuantity = datum.take(1).toInt()
                         val child = LuggageNode(LuggageData(childColour, childQuantity))
-                        if (existing.contains(child)) {
-                            addChild(existing.find { it == child }!!)
+                        if (existingChildren.contains(child)) {
+                            addChild(existingChildren.find { it == child }!!)
                         } else {
                             addChild(child)
-                            existing.add(child)
                         }
                     }
                 }
-            }.toMutableList()
-
-            val roots = nodes.fold(mutableListOf<LuggageNode>()) { roots, node ->
-                roots.apply {
-                    if (!nodes.flatMap { it.children.map { it.value.colour } }.contains(node.data.colour)) {
-                        add(node)
-                    }
-                }
-            }
-
-            var next = roots
-            val evaluated = mutableSetOf<LuggageNode>()
-
-            while(next.isNotEmpty()) {
-                val childrenAdded = mutableListOf<LuggageNode>()
-                next.forEach { parentCandidate ->
-                    val childColours = parentCandidate.children.map { it.value.colour }
-                    val children = nodes.filter { candidate ->
-                        childColours.contains(candidate.data.colour)
-                    }
-                    children.forEach { child ->
-                        val grandChildren = child.children
-                        parentCandidate.children.forEach { fakeChild ->
-                            val actualChild = existing.find { it.value.colour == fakeChild.value.colour }!!
-                            if (!evaluated.contains(actualChild)) {
-                                grandChildren.forEach { actualChild.addChild(it) }
-                                evaluated.add(actualChild as LuggageNode)
-                                childrenAdded.add(actualChild)
-                            }
-                        }
-                    }
-                }
-                next = childrenAdded.distinct().toMutableList()
-            }
-
-            return nodes.first()
-
+                if (node.parents.size == 0) existing.add(node)
+                node
+            }.filter { it.isRoot() }.toSet()
         }
     }
 }
