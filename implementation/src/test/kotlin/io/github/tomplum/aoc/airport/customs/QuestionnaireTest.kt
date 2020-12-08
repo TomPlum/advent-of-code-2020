cf7b5f78dc8097a09908751f8e9a061d117969a2
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-08 18:27:58 +0000

    Day 4 | Questionnaire Branch Coverage

--- implementation/src/test/kotlin/io/github/tomplum/aoc/airport/customs/QuestionnaireTest.kt
+++ implementation/src/test/kotlin/io/github/tomplum/aoc/airport/customs/QuestionnaireTest.kt
@@ -3,20 +3,37 @@ package io.github.tomplum.aoc.airport.customs
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import io.github.tomplum.aoc.input.TestInputReader
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class QuestionnaireTest {
-    @Test
-    fun getCorrectQuestionCountExample() {
-        val input = TestInputReader().readInputAsString("customs/example-answers.txt").asSingleString()
-        val count = Questionnaire(input).getYesQuestionCount()
-        assertThat(count).isEqualTo(11)
+    @Nested
+    inner class YesQuestionCount {
+        @Test
+        fun example() {
+            val input = TestInputReader().readInputAsString("customs/example-answers.txt").asSingleString()
+            val count = Questionnaire(input).getYesQuestionCount()
+            assertThat(count).isEqualTo(11)
+        }
+
+        @Test
+        fun emptyAnswers() {
+            assertThat(Questionnaire("").getYesQuestionCount()).isEqualTo(0)
+        }
     }
 
-    @Test
-    fun getCommonCorrectQuestionCountExample() {
-        val input = TestInputReader().readInputAsString("customs/example-answers.txt").asSingleString()
-        val count = Questionnaire(input).getCommonYesQuestionCount()
-        assertThat(count).isEqualTo(6)
+    @Nested
+    inner class CommonYesQuestionCount {
+        @Test
+        fun example() {
+            val input = TestInputReader().readInputAsString("customs/example-answers.txt").asSingleString()
+            val count = Questionnaire(input).getCommonYesQuestionCount()
+            assertThat(count).isEqualTo(6)
+        }
+
+        @Test
+        fun emptyAnswers() {
+            assertThat(Questionnaire("").getCommonYesQuestionCount()).isEqualTo(0)
+        }
     }
 }
