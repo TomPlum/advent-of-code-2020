18a57cd9e09dfb8a50d6eff0ecad839bd0ea7cdd
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-09 11:06:18 +0000

    Day 9 | Added Solution Unit Tests

---
+++ solutions/src/test/kotlin/io/github/tomplum/aoc/solutions/Day9Test.kt
@@ -0,0 +1,17 @@
+package io.github.tomplum.aoc.solutions
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+
+class Day9Test {
+    @Test
+    fun partOne() {
+        assertThat(Day9().part1()).isEqualTo(20874512)
+    }
+
+    @Test
+    fun partTwo() {
+        assertThat(Day9().part1()).isEqualTo(3012420)
+    }
+}
