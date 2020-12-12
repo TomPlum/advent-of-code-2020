700b1afe2ff7eaaee56c551560a16dce6309752f
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-12 14:16:39 +0000

    Day 12 | Navigation System - Extracted Redundancy / Language Consistency

--- implementation/src/main/kotlin/io/github/tomplum/aoc/ferry/navigation/NavigationSystem.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/ferry/navigation/NavigationSystem.kt
@@ -10,87 +10,94 @@ import kotlin.math.sin
 class NavigationSystem(data: List<String>) {
     private val instructions: List<Instruction<*>>
     private var shipsDirection = Direction.RIGHT
-    private var shipsPosition = Point2D(0,0)
-    private var waypointPosition = Point2D(10, 1)
+    private var ship = Point2D(0, 0)
+    private var waypoint = Point2D(10, 1)
 
     init {
         instructions = data.map {
-            when(it.take(1)) {
-                "N" -> Instruction(Direction.UP, it.drop(1).toInt())
-                "S" -> Instruction(Direction.DOWN, it.drop(1).toInt())
-                "E" -> Instruction(Direction.RIGHT, it.drop(1).toInt())
-                "W" -> Instruction(Direction.LEFT, it.drop(1).toInt())
-                "L" -> Instruction(Directive.LEFT, it.drop(1).toInt())
-                "R" -> Instruction(Directive.RIGHT, it.drop(1).toInt())
-                "F" -> Instruction(Directive.FORWARD, it.drop(1).toInt())
+            val value = it.drop(1).toInt()
+            when (it.take(1)) {
+                "N" -> Instruction(Direction.UP, value)
+                "S" -> Instruction(Direction.DOWN, value)
+                "E" -> Instruction(Direction.RIGHT, value)
+                "W" -> Instruction(Direction.LEFT, value)
+                "L" -> Instruction(Directive.LEFT, value)
+                "R" -> Instruction(Directive.RIGHT, value)
+                "F" -> Instruction(Directive.FORWARD, value)
                 else -> throw IllegalArgumentException("Invalid Instruction $it")
             }
         }
     }
 
     fun navigate(): Int {
-        instructions.forEach { instruction ->
-            when(instruction.action) {
-                is Direction -> shipsPosition = shipsPosition.shift(instruction.action, instruction.value)
-                is Directive -> when(instruction.action) {
-                    Directive.LEFT -> when(instruction.value) {
+        instructions.forEach { (action, value) ->
+            when (action) {
+                is Direction -> ship = ship.shift(action, value)
+                is Directive -> when (action) {
+                    Directive.LEFT -> when (value) {
                         90 -> shipsDirection = shipsDirection.rotateAntiClockwise()
                         180 -> repeat(2) { shipsDirection = shipsDirection.rotateAntiClockwise() } //TODO: Update Point2D to rotate 90,180,270
                         270 -> repeat(3) { shipsDirection = shipsDirection.rotateAntiClockwise() }
                     }
-                    Directive.RIGHT -> when(instruction.value) {
+                    Directive.RIGHT -> when (value) {
                         90 -> shipsDirection = shipsDirection.rotateClockwise90()
                         180 -> repeat(2) { shipsDirection = shipsDirection.rotateClockwise90() }
                         270 -> repeat(3) { shipsDirection = shipsDirection.rotateClockwise90() }
                     }
-                    Directive.FORWARD -> shipsPosition = shipsPosition.shift(shipsDirection, instruction.value)
+                    Directive.FORWARD -> ship = ship.shift(shipsDirection, value)
                 }
             }
         }
-        return Point2D(0,0).distanceBetween(shipsPosition)
+        return Point2D(0, 0).distanceBetween(ship)
     }
 
-    fun navigateViaWaypoint(): Int {
-        instructions.forEach { instruction ->
-            when(instruction.action) {
-                is Direction -> waypointPosition = waypointPosition.shift(instruction.action, instruction.value)
-                is Directive -> when(instruction.action) {
-                    Directive.LEFT -> {
-                        waypointPosition = waypointPosition.rotateAbout(shipsPosition, -instruction.value)
-                    }
-                    Directive.RIGHT -> {
-                        waypointPosition = waypointPosition.rotateAbout(shipsPosition, instruction.value)
-                    }
-                    Directive.FORWARD -> {
-                        val xDelta = waypointPosition.x - shipsPosition.x
-                        val yDelta = waypointPosition.y - shipsPosition.y
-                        repeat(instruction.value) {
-                            when {
-                                xDelta > 0 -> {
-                                    shipsPosition = shipsPosition.shift(Direction.RIGHT, xDelta)
-                                    waypointPosition = waypointPosition.shift(Direction.RIGHT, xDelta)
-                                }
-                                xDelta < 0 -> {
-                                    shipsPosition = shipsPosition.shift(Direction.LEFT, abs(xDelta))
-                                    waypointPosition = waypointPosition.shift(Direction.LEFT, abs(xDelta))
-                                }
-                            }
-                            when {
-                                yDelta > 0 -> {
-                                    shipsPosition = shipsPosition.shift(Direction.UP, yDelta)
-                                    waypointPosition = waypointPosition.shift(Direction.UP, yDelta)
-                                }
-                                yDelta < 0 -> {
-                                    shipsPosition = shipsPosition.shift(Direction.DOWN, abs(yDelta))
-                                    waypointPosition = waypointPosition.shift(Direction.DOWN, abs(yDelta))
-                                }
-                            }
-                        }
-                    }
+    fun navigateViaWaypoint(): Int = instructions.forEach { (action, value) ->
+        when (action) {
+            is Direction -> moveWaypoint(action, value)
+            is Directive -> when (action) {
+                Directive.LEFT -> rotateWaypoint(-value)
+                Directive.RIGHT -> rotateWaypoint(value)
+                Directive.FORWARD -> {
+                    val (xDirection, xUnits) = waypoint.xRelativeDirection(ship) ?: (null to 0)
+                    val (yDirection, yUnits) = waypoint.yRelativeDirection(ship) ?: (null to 0)
+                    moveShip(xDirection, xUnits * value)
+                    moveShip(yDirection, yUnits * value)
                 }
             }
         }
-        return Point2D(0,0).distanceBetween(shipsPosition)
+    }.let { Point2D(0, 0).distanceBetween(ship) }
+
+    private fun rotateWaypoint(angle: Int) {
+        waypoint = waypoint.rotateAbout(ship, angle)
+    }
+
+    private fun moveShip(direction: Direction?, units: Int) {
+        if (direction != null) {
+            ship = ship.shift(direction, units)
+            moveWaypoint(direction, units)
+        }
+    }
+
+    private fun moveWaypoint(direction: Direction, units: Int) {
+        waypoint = waypoint.shift(direction, units)
+    }
+
+    private fun Point2D.xRelativeDirection(other: Point2D): Pair<Direction, Int>? {
+        val xDelta = x - other.x
+        return when {
+            xDelta > 0 -> Pair(Direction.RIGHT, xDelta)
+            xDelta < 0 -> Pair(Direction.LEFT, abs(xDelta))
+            else -> null
+        }
+    }
+
+    private fun Point2D.yRelativeDirection(other: Point2D): Pair<Direction, Int>? {
+        val yDelta = y - other.y
+        return when {
+            yDelta > 0 -> Pair(Direction.UP, yDelta)
+            yDelta < 0 -> Pair(Direction.DOWN, abs(yDelta))
+            else -> null
+        }
     }
 
     private fun Point2D.rotateAbout(pivot: Point2D, angle: Int = 90): Point2D {
