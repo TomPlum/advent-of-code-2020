f8e9ab5f79ae7701db0c15f909f1ead20b14393d
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-12 18:38:23 +0000

    Day 12 | Added Instruction Parser Unit Test Suite

--- implementation/src/main/kotlin/io/github/tomplum/aoc/ferry/navigation/InstructionParser.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/ferry/navigation/InstructionParser.kt
@@ -4,9 +4,9 @@ import io.github.tomplum.libs.math.Direction
 
 class InstructionParser private constructor() {
     companion object {
-        fun parse(data: List<String>) = data.map {
-            val value = it.drop(1).toInt()
-            when (it.take(1)) {
+        fun parse(data: List<String>) = data.map { instruction ->
+            val value = instruction.drop(1).toInt()
+            when (instruction.take(1)) {
                 "N" -> Instruction(Direction.UP, value)
                 "S" -> Instruction(Direction.DOWN, value)
                 "E" -> Instruction(Direction.RIGHT, value)
@@ -14,7 +14,7 @@ class InstructionParser private constructor() {
                 "L" -> Instruction(Directive.LEFT, value)
                 "R" -> Instruction(Directive.RIGHT, value)
                 "F" -> Instruction(Directive.FORWARD, value)
-                else -> throw IllegalArgumentException("Invalid Instruction $it")
+                else -> throw IllegalArgumentException("Invalid Instruction $instruction")
             }
         }
     }
--- implementation/src/test/kotlin/io/github/tomplum/aoc/ferry/navigation/InstructionParserTest.kt
+++ implementation/src/test/kotlin/io/github/tomplum/aoc/ferry/navigation/InstructionParserTest.kt
@@ -1,12 +1,58 @@
 package io.github.tomplum.aoc.ferry.navigation
 
 import assertk.assertThat
+import assertk.assertions.containsExactly
 import assertk.assertions.isEqualTo
 import io.github.tomplum.aoc.input.TestInputReader
 import io.github.tomplum.libs.math.Direction
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class InstructionParserTest {
+    @Test
+    fun north() {
+        assertThat(InstructionParser.parse(listOf("N2"))).containsExactly(Instruction(Direction.UP, 2))
+    }
+
+    @Test
+    fun south() {
+        assertThat(InstructionParser.parse(listOf("S5"))).containsExactly(Instruction(Direction.DOWN, 5))
+    }
+
+    @Test
+    fun east() {
+        assertThat(InstructionParser.parse(listOf("E10"))).containsExactly(Instruction(Direction.RIGHT, 10))
+    }
+
+    @Test
+    fun west() {
+        assertThat(InstructionParser.parse(listOf("W6"))).containsExactly(Instruction(Direction.LEFT, 6))
+    }
+
+    @Test
+    fun left() {
+        assertThat(InstructionParser.parse(listOf("L90"))).containsExactly(Instruction(Directive.LEFT, 90))
+    }
+
+    @Test
+    fun right() {
+        assertThat(InstructionParser.parse(listOf("R180"))).containsExactly(Instruction(Directive.RIGHT, 180))
+    }
+
+    @Test
+    fun forward() {
+        assertThat(InstructionParser.parse(listOf("F11"))).containsExactly(Instruction(Directive.FORWARD, 11))
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["J1", "H10", "T1"])
+    fun invalidValue(value: String) {
+        val e = assertThrows<IllegalArgumentException> { InstructionParser.parse(listOf(value)) }
+        assertThat(e.message).isEqualTo("Invalid Instruction $value")
+    }
+
     @Test
     fun example() {
         val data = TestInputReader.read<String>("ferry/navigation/instructions.txt").value
