d6852317b9a203dbafca693036222fd8f976071f
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-13 18:51:56 +0000

    Day 13 | Added Kotlin Doc

--- implementation/src/main/kotlin/io/github/tomplum/aoc/bus/Bus.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/bus/Bus.kt
@@ -1,7 +1,20 @@
 package io.github.tomplum.aoc.bus
 
+/**
+ * A shuttle bus for transporting passengers to the airport.
+ * @param id The unique ID of the bus. Can be 'x' if out-of-service.
+ */
 data class Bus(private val id: String) {
+    /**
+     * Checks if the bus is out-of-service.
+     * @return true if out-of-service, false if running.
+     */
     fun isOutOfService(): Boolean = id == "x"
 
+    /**
+     * Gets the unique ID of the bus.
+     * @throws IllegalStateException if the bus is out-of-service.
+     * @return The ID
+     */
     fun getID(): Int = if (!isOutOfService()) id.toInt() else throw IllegalStateException("This bus is out of service!")
 }
--- implementation/src/main/kotlin/io/github/tomplum/aoc/bus/BusScheduler.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/bus/BusScheduler.kt
@@ -1,6 +1,19 @@
 package io.github.tomplum.aoc.bus
 
+/**
+ * A scheduler for the shuttle bus service.
+ *
+ * Bus schedules are defined based on a timestamp that measures the number of minutes since some fixed reference point
+ * in the past. At timestamp 0, every bus simultaneously departed from the sea port. After that, each bus travels to
+ * the airport, then various other locations, and finally returns to the sea port to repeat its journey forever.
+ *
+ * @param timetable A table of bus departure times
+ */
 class BusScheduler(private val timetable: BusTimetable) {
+    /**
+     * Finds the [Bus] which departs the shortest time after the [timetable] arrival time.
+     * @return The product of the [Bus] ID and the waiting time.
+     */
     fun getEarliestBus(): Int {
         val buses = timetable.getWorkingBuses()
         val firstAvailableTimes = buses.map { bus -> bus to bus.getID().getLastArrivalTime() + bus.getID() }.toMap()
@@ -9,6 +22,16 @@ class BusScheduler(private val timetable: BusTimetable) {
         return best.key.getID() * best.value
     }
 
+    /**
+     * Finds the timestamp in which all the buses depart at their given offsets as defined in the [timetable].
+     * @see [BusTimetable.getBusesWithOffsets].
+     *
+     * For each bus, the timestamp (t) should satisfy: t + offset % id == 0
+     * Once the timestamp has been found for a bus, the step ratio can be increased by a factory of the previous id.
+     * This means each of the previous equations will be satisfied and skip lots of unnecessary processing.
+     *
+     * @return The timestamp that satisfies all departures at their offsets.
+     */
     fun getOffsetDepartureTime(): Long {
         val buses = timetable.getBusesWithOffsets()
         var step = buses.first().second.toLong()
