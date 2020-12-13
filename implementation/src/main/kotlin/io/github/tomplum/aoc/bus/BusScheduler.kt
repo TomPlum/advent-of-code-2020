52e887c9fa448e52039945bdb8d6a87dc44873ad
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-13 17:33:20 +0000

    Day 13 | Implemented Offset Departure Time Algo & Proved Several Example Inputs

--- implementation/src/main/kotlin/io/github/tomplum/aoc/bus/BusScheduler.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/bus/BusScheduler.kt
@@ -9,5 +9,18 @@ class BusScheduler(private val notes: BusTimetable) {
         return best.key.getValue() * best.value
     }
 
+    fun getOffsetDepartureTime(): Long {
+        val buses = notes.buses.mapIndexedNotNull { i, id -> if (id.isOutOfService()) null else i to id.getValue() }
+        var step = buses.first().second
+        var t = 0L
+        buses.drop(1).forEach { (i, bus) ->
+            while ((t + i) % bus != 0L) {
+                t += step
+            }
+            step *= bus
+        }
+        return t
+    }
+
     private fun Int.getLastArrivalTime() = notes.arrivalTime - (notes.arrivalTime % this)
 }
--- implementation/src/test/kotlin/io/github/tomplum/aoc/bus/BusSchedulerTest.kt
+++ implementation/src/test/kotlin/io/github/tomplum/aoc/bus/BusSchedulerTest.kt
@@ -3,13 +3,62 @@ package io.github.tomplum.aoc.bus
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import io.github.tomplum.aoc.input.TestInputReader
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class BusSchedulerTest {
-    @Test
-    fun example() {
-        val input = TestInputReader.read<String>("bus/example-notes.txt")
-        val scheduler = BusScheduler(BusTimetable.fromNotes(input.value))
-        assertThat(scheduler.getEarliestBus()).isEqualTo(295)
+    @Nested
+    inner class GetEarliestBus {
+        @Test
+        fun example() {
+            val input = TestInputReader.read<String>("bus/example-notes.txt")
+            val scheduler = BusScheduler(BusTimetable.fromNotes(input.value))
+            assertThat(scheduler.getEarliestBus()).isEqualTo(295)
+        }
+    }
+
+    @Nested
+    inner class GetOffsetDepartureTimestamp {
+        @Test
+        fun exampleOne() {
+            val input = TestInputReader.read<String>("bus/example-notes.txt")
+            val scheduler = BusScheduler(BusTimetable.fromNotes(input.value))
+            assertThat(scheduler.getOffsetDepartureTime()).isEqualTo(1068781)
+        }
+
+        @Test
+        fun exampleTwo() {
+            val input = TestInputReader.read<String>("bus/example-notes-2.txt")
+            val scheduler = BusScheduler(BusTimetable.fromNotes(input.value))
+            assertThat(scheduler.getOffsetDepartureTime()).isEqualTo(3417)
+        }
+
+        @Test
+        fun exampleThree() {
+            val input = TestInputReader.read<String>("bus/example-notes-3.txt")
+            val scheduler = BusScheduler(BusTimetable.fromNotes(input.value))
+            assertThat(scheduler.getOffsetDepartureTime()).isEqualTo(754018)
+        }
+
+        @Test
+        fun exampleFour() {
+            val input = TestInputReader.read<String>("bus/example-notes-4.txt")
+            val scheduler = BusScheduler(BusTimetable.fromNotes(input.value))
+            assertThat(scheduler.getOffsetDepartureTime()).isEqualTo(779210)
+        }
+
+        @Test
+        fun exampleFive() {
+            val input = TestInputReader.read<String>("bus/example-notes-5.txt")
+            val scheduler = BusScheduler(BusTimetable.fromNotes(input.value))
+            assertThat(scheduler.getOffsetDepartureTime()).isEqualTo(1261476)
+        }
+
+        @Test
+        fun exampleSix() {
+            val input = TestInputReader.read<String>("bus/example-notes-6.txt")
+            val scheduler = BusScheduler(BusTimetable.fromNotes(input.value))
+            assertThat(scheduler.getOffsetDepartureTime()).isEqualTo(1202161486)
+        }
     }
 }
