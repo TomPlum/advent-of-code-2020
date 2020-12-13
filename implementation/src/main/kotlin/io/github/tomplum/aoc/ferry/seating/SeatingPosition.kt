a22a88c916da4b97587da7b0002baa7a21a88933
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-13 08:00:45 +0000

    Day 11 | Improved Performance w/Parallel Streams (22s Total -> ~9s)

--- implementation/src/main/kotlin/io/github/tomplum/aoc/ferry/seating/SeatingPosition.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/ferry/seating/SeatingPosition.kt
@@ -2,7 +2,7 @@ package io.github.tomplum.aoc.ferry.seating
 
 import io.github.tomplum.libs.math.map.MapTile
 
-class SeatingPosition(val status: Char): MapTile<Char>(status) {
+class SeatingPosition(private val status: Char): MapTile<Char>(status) {
     fun isFloor(): Boolean = status == '.'
 
     fun isEmpty(): Boolean = status == 'L'
--- implementation/src/main/kotlin/io/github/tomplum/aoc/ferry/seating/strategy/InitialSeatingStrategy.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/ferry/seating/strategy/InitialSeatingStrategy.kt
@@ -2,17 +2,18 @@ package io.github.tomplum.aoc.ferry.seating.strategy
 
 import io.github.tomplum.aoc.ferry.seating.SeatingLayout
 import io.github.tomplum.libs.math.Point2D
+import java.util.stream.Collectors
 
 class InitialSeatingStrategy : SeatingStrategy {
     override fun getOccupiedSeatPositions(layout: SeatingLayout): Set<Point2D> {
-        return layout.getSeats { it.isEmpty() }.filterKeys { pos ->
+        return layout.getSeats { it.isEmpty() }.keys.parallelStream().filter { pos ->
             layout.getAdjacentSeating(pos).values.none { it!!.isOccupied() }
-        }.keys
+        }.collect(Collectors.toSet())
     }
 
     override fun getEmptySeatPositions(layout: SeatingLayout): Set<Point2D> {
-        return layout.getSeats { it.isOccupied() }.filterKeys { pos ->
+        return layout.getSeats { it.isOccupied() }.keys.parallelStream().filter { pos ->
             layout.getAdjacentSeating(pos).values.count { it!!.isOccupied() } >= 4
-        }.keys
+        }.collect(Collectors.toSet())
     }
 }
