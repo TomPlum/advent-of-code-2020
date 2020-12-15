85e3b7d7e85fb11dcdc9d1223601e39573c4d26e
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-15 09:02:59 +0000

    Day 15 | Completed Part 1 & Tested Solution

---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day15.kt
@@ -0,0 +1,14 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.Solution
+import io.github.tomplum.aoc.airport.game.MemoryGame
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+
+class Day15 : Solution<Int, Int> {
+    private val input = InputReader.read<String>(Day(15)).asSingleString()
+
+    override fun part1(): Int {
+        return MemoryGame(input.split(",").map { it.toInt() }).simulate(2020)
+    }
+}
---
+++ solutions/src/test/kotlin/io/github/tomplum/aoc/solutions/Day15Test.kt
@@ -0,0 +1,12 @@
+package io.github.tomplum.aoc.solutions
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+
+class Day15Test {
+    @Test
+    fun partOne() {
+        assertThat(Day15().part1()).isEqualTo(1259)
+    }
+}
