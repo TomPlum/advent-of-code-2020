ad3456c71e07dcdce27040fa10a72872819d376a
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-15 21:32:47 +0000

    Day 15 | Refactored Memory Game Logic (Current vs Last Number) - Improved Performance ~200%

--- implementation/src/main/kotlin/io/github/tomplum/aoc/airport/game/MemoryGame.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/airport/game/MemoryGame.kt
@@ -11,46 +11,36 @@ package io.github.tomplum.aoc.airport.game
  * - Otherwise, the number had been spoken before; the current player announces how many turns apart the number is
  *   from when it was previously spoken.
  *
- * @param numbers The starting numbers.
+ * @param data The starting numbers.
  */
-class MemoryGame(numbers: String) {
-    private val turnAsked = mutableMapOf<Int, IntArray>()
-    private val timesAsked = mutableMapOf<Int, Int>()
-    private var lastNumber = 0
-    private var turn = 1
+class MemoryGame(data: String) {
+    private val spoken = HashMap<Int, Int>()
+    private var currentNumber: Int
+    private var turn: Int
 
     init {
-        numbers.split(",").map { it.toInt() }.forEach { number -> speak(number) }
+        val numbers = data.split(",").map { it.toInt() }
+        numbers.forEachIndexed { i, number -> spoken[number] = i + 1 }
+        currentNumber = numbers.last()
+        turn = numbers.size
     }
 
     /**
      * Simulates the memory game up until the given [lastTurn].
-     * @return The [lastNumber] spoken when the simulation ends.
+     * @return The [currentNumber] spoken when the simulation ends.
      */
     fun simulate(lastTurn: Int): Int {
-        while (turn <= lastTurn) {
-            if (timesAsked[lastNumber] == 1) {
-               speak(0)
-            } else {
-                val lastTwoTurns = turnAsked[lastNumber]!!.takeLast(2)
-                val diff = lastTwoTurns[1] - lastTwoTurns[0]
-                speak(diff)
+        while (turn < lastTurn) {
+            when {
+                !spoken.containsKey(currentNumber) -> speak(0)
+                else -> speak(turn - spoken[currentNumber]!!)
             }
         }
-        return lastNumber
+        return currentNumber
     }
 
     private fun speak(number: Int) {
-        timesAsked[number] = timesAsked.getOrDefault(number, 0) + 1
-        lastNumber = number
-        setTurnAsked(number)
-        turn++
-    }
-
-    private fun setTurnAsked(number: Int) {
-        val turns = turnAsked.getOrDefault(number, IntArray(2) { 0 })
-        turns[0] = turns[1]
-        turns[1] = turn
-        turnAsked[number] = turns
+        spoken[currentNumber] = turn++
+        currentNumber = number
     }
 }
