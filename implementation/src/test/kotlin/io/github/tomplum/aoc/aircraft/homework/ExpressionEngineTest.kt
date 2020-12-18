d55752558e7b5e1330751a81aa90cf0c6f0376ba
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-18 16:40:53 +0000

    Day 18 | Moved Unit Tests To Correct Classes & Added More Examples

--- implementation/src/test/kotlin/io/github/tomplum/aoc/aircraft/homework/ExpressionEngineTest.kt
+++ implementation/src/test/kotlin/io/github/tomplum/aoc/aircraft/homework/ExpressionEngineTest.kt
@@ -3,6 +3,7 @@ package io.github.tomplum.aoc.aircraft.homework
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import io.github.tomplum.aoc.aircraft.homework.strategy.BasicMath
+import io.github.tomplum.aoc.input.TestInputReader
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
@@ -10,38 +11,11 @@ class ExpressionEngineTest {
     @Nested
     inner class BasicMathExamples {
         @Test
-        fun exampleOne() {
-            val expressions = Lexer().read(listOf("1 + 2 * 3 + 4 * 5 + 6"))
+        fun example() {
+            val input = TestInputReader.read<String>("homework/example.txt")
+            val expressions = Lexer().read(input.value)
             val engine = ExpressionEngine(BasicMath())
-            assertThat(engine.sum(expressions)).isEqualTo(71)
-        }
-
-        @Test
-        fun exampleTwo() {
-            val expressions = Lexer().read(listOf("2 * 3 + (4 * 5)"))
-            val engine = ExpressionEngine(BasicMath())
-            assertThat(engine.sum(expressions)).isEqualTo(26)
-        }
-
-        @Test
-        fun exampleThree() {
-            val expressions = Lexer().read(listOf("5 + (8 * 3 + 9 + 3 * 4 * 3)"))
-            val engine = ExpressionEngine(BasicMath())
-            assertThat(engine.sum(expressions)).isEqualTo(437)
-        }
-
-        @Test
-        fun exampleFour() {
-            val expressions = Lexer().read(listOf("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"))
-            val engine = ExpressionEngine(BasicMath())
-            assertThat(engine.sum(expressions)).isEqualTo(12240)
-        }
-
-        @Test
-        fun exampleFive() {
-            val expressions = Lexer().read(listOf("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))
-            val engine = ExpressionEngine(BasicMath())
-            assertThat(engine.sum(expressions)).isEqualTo(13632)
+            assertThat(engine.sum(expressions)).isEqualTo(26386)
         }
     }
 }
--- implementation/src/test/kotlin/io/github/tomplum/aoc/aircraft/homework/ExpressionTest.kt
+++ implementation/src/test/kotlin/io/github/tomplum/aoc/aircraft/homework/ExpressionTest.kt
@@ -8,29 +8,5 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class ExpressionTest {
-    @Nested
-    inner class SolveBasicMath {
-        @Test
-        fun multiply() {
-            val expression = Expression(listOf(Number(2), MULTIPLY, Number(3)))
-            val result = expression.solve(BasicMath())
-            assertThat(result.value).isEqualTo(6)
-        }
 
-        @Test
-        fun addition() {
-            val expression = Expression(listOf(Number(2), ADD, Number(3)))
-            val result = expression.solve(BasicMath())
-            assertThat(result.value).isEqualTo(5)
-        }
-
-        @Test
-        fun nested() {
-            val expression = Expression(
-                listOf(Number(1), ADD, Expression(listOf(Number(2), MULTIPLY, Number(3))))
-            )
-            val result = expression.solve(BasicMath())
-            assertThat(result.value).isEqualTo(7)
-        }
-    }
 }
