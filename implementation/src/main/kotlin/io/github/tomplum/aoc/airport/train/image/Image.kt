b4d4c412e1227f003d82ce6e3ef1783945030966
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-28 09:14:30 +0000

    Day 20 | Image - Removed Redundant Width Calculations (-10s Solution Runtime)

--- implementation/src/main/kotlin/io/github/tomplum/aoc/airport/train/image/Image.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/airport/train/image/Image.kt
@@ -5,7 +5,7 @@ import io.github.tomplum.libs.math.map.AdventMap2D
 import io.github.tomplum.libs.math.point.Point2D
 import kotlin.math.abs
 
-class Image: AdventMap2D<ImageTileData>() {
+class Image(var width: Int = 0) : AdventMap2D<ImageTileData>() {
 
     companion object {
         fun assembleFromMapping(mapping: ImageTileMapping): Image {
@@ -28,6 +28,7 @@ class Image: AdventMap2D<ImageTileData>() {
                 }
             }
 
+            image.width = y
             return image.locateSeaMonsters()
         }
     }
@@ -48,8 +49,8 @@ class Image: AdventMap2D<ImageTileData>() {
     fun getHabitatWaterRoughness(): Int = filterTiles { tile -> tile.isWave() }.count()
 
     fun locateSeaMonsters(): Image {
-        (1 until yMax()!!).forEach { y ->
-            (0..xMax()!! - 19).forEach { x ->
+        (1 until width).forEach { y ->
+            (0..width - 19).forEach { x ->
                 val position = Point2D(x, y)
                 val positions = position.getSeaMonsterPositionCandidates()
                 val isMatch = filterPoints(positions).values.all { tile -> tile.isWave() }
@@ -65,25 +66,24 @@ class Image: AdventMap2D<ImageTileData>() {
 
     fun containsSeaMonsters(): Boolean = filterTiles { it.isSeaMonster() }.count() > 0
 
-    private fun xFlip(): Image = data.entries.fold(Image()) { flipped, (pos, tile) ->
-        val posFlipped = Point2D(xMax()!! - pos.x, pos.y)
+    private fun xFlip(): Image = data.entries.fold(Image(width)) { flipped, (pos, tile) ->
+        val posFlipped = Point2D(width - pos.x, pos.y)
         flipped.apply { addTile(posFlipped, tile) }
     }
 
 
-    private fun yFlip(): Image = data.entries.fold(Image()) { flipped, (pos, tile) ->
-        val posFlipped = Point2D(pos.x, yMax()!! - pos.y)
+    private fun yFlip(): Image = data.entries.fold(Image(width)) { flipped, (pos, tile) ->
+        val posFlipped = Point2D(pos.x, width - pos.y)
         flipped.apply { addTile(posFlipped, tile) }
     }
 
-    private fun rotateClockwise(degrees: Int): Image = data
-        .entries.fold(Image()) { rotated, (pos, tile) ->
-            val yInverted = Point2D(pos.x, -pos.y) //Convert regular cartesian -> single quadrant (y is down)
-            val posRotated = yInverted.rotateAbout(Point2D.origin(), degrees) //Rotate about origin
-            val posShifted = posRotated.shift(RIGHT, xMax()!!) //Shift back into our quadrant
-            val yCorrected = Point2D(posShifted.x, abs(posShifted.y)) //Flip y back to our quadrant
-            rotated.apply { addTile(yCorrected, tile) }
-        }
+    private fun rotateClockwise(degrees: Int): Image = data.entries.fold(Image(width)) { rotated, (pos, tile) ->
+        val yInverted = Point2D(pos.x, -pos.y) //Convert regular cartesian -> single quadrant (y is down)
+        val posRotated = yInverted.rotateAbout(Point2D.origin(), degrees) //Rotate about origin
+        val posShifted = posRotated.shift(RIGHT, width) //Shift back into our quadrant
+        val yCorrected = Point2D(posShifted.x, abs(posShifted.y)) //Flip y back to our quadrant
+        rotated.apply { addTile(yCorrected, tile) }
+    }
 
     private fun Point2D.getSeaMonsterPositionCandidates(): Set<Point2D> {
         val first = this
@@ -101,7 +101,9 @@ class Image: AdventMap2D<ImageTileData>() {
         val thirteenth = twelfth.shift(DOWN, 1).shift(RIGHT, 1)
         val fourteenth = thirteenth.shift(UP, 1)
         val fifteenth = fourteenth.shift(RIGHT, 1)
-        return setOf(first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth,
-            eleventh, twelfth, thirteenth, fourteenth, fifteenth)
+        return setOf(
+            first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth,
+            eleventh, twelfth, thirteenth, fourteenth, fifteenth
+        )
     }
 }
