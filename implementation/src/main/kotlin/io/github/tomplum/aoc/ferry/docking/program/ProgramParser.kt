bdf109dd55e4672186008bf1b638768ee4d8bbf5
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-01-10 19:35:48 +0000

    Day 14 | Added Explicit Lambda Naming & Missing Kotlin Doc

--- implementation/src/main/kotlin/io/github/tomplum/aoc/ferry/docking/program/ProgramParser.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/ferry/docking/program/ProgramParser.kt
@@ -5,15 +5,26 @@ package io.github.tomplum.aoc.ferry.docking.program
  */
 class ProgramParser private constructor() {
     companion object {
+        /**
+         * Parses the initialisation program instructions.
+         *
+         * mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+         * mem[8] = 11
+         * mem[7] = 101
+         * mem[8] = 0
+         *
+         * @param data A list of input data in the above format.
+         * @return The docking initialisation program
+         */
         fun parse(data: List<String>): InitProgram {
             val programData = mutableMapOf<BitMask, MutableList<Instruction>>()
             var mask = BitMask("")
-            data.forEach {
-                if (it.take(4) == "mask") {
-                    mask = BitMask(it.takeLast(36))
+            data.forEach { line ->
+                if (line.take(4) == "mask") {
+                    mask = BitMask(line.takeLast(36))
                     programData[mask] = mutableListOf()
                 } else {
-                    val info = it.split(" = ")
+                    val info = line.split(" = ")
                     val address = info[0].removePrefix("mem[").removeSuffix("]").toInt()
                     val value = info[1].toInt()
                     programData[mask]?.add(Instruction(address, value))
